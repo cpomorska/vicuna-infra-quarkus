a460d5a530b1826406cdfd8c83d9d1fc0ccb4d88
cpomorska <claudius.pomorska@scprojekt.de>
2022-04-24 23:15:15 +0200

    Prepared for Jakarta Rewrite (#1)
    * Prepared for Jakarta Rewrite
    
    * modified changes
    
    Co-authored-by: Claudius Pomorska <claudius.pomorska@scprojekt.com>

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module vicuna.infra.quarkus {
+    requires vicuna.core.lib;
+    requires java.persistence;
+    requires java.transaction;
+    requires lombok;
+    requires jakarta.inject.api;
+    requires jakarta.enterprise.cdi.api;
+    requires quarkus.hibernate.orm;
+    requires jakarta.persistence;
+}
